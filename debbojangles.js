576a7be9a353a76b60da36cff16ce4086306cf92
goobergirl87 <isabelheidekorn@gmail.com>
2023-04-13 08:29:47 +0200

    Add scroll function and photo sketch pad

---
+++ debbojangles.js
@@ -0,0 +1,25 @@
+let start = document.querySelector("#start")
+let pageBottom = document.querySelector("#page-bottom")
+
+start.addEventListener("click", function() {
+  pageBottom.scrollIntoView()
+})
+
+
+
+for (let i = 0; i < 120; i++){
+        const photoSketchPad = document.querySelector('#photoSketchPad');
+        const square = document.createElement('div');
+        square.setAttribute('style', 'display: flex; height: 19.155px; width: 19.155px');
+        square.classList.add('square');
+        photoSketchPad.appendChild(square);
+
+        square.onmousemove = function(){
+            square.style.backgroundColor = 'transparent';
+
+
+        };
+
+};
+
+
