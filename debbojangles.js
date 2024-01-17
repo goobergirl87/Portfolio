72a333b56c93453b12a8b2318d707f06d13a71b8
goobergirl87 <isabelheidekorn@gmail.com>
2024-01-17 11:13:37 +0100

    Add smooth scrolling

--- debbojangles.js
+++ debbojangles.js
@@ -1,25 +1,40 @@
-let start = document.querySelector("#start")
-let pageBottom = document.querySelector("#page-bottom")
+// let start = document.querySelector("#start");
+// let pageBottom = document.querySelector("#page-bottom");
 
-start.addEventListener("click", function() {
-  pageBottom.scrollIntoView()
-})
+// start.addEventListener("click", function () {
+//   pageBottom.scrollIntoView();
+// });
 
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
 
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
 
-for (let i = 0; i < 120; i++){
-        const photoSketchPad = document.querySelector('#photoSketchPad');
-        const square = document.createElement('div');
-        square.setAttribute('style', 'display: flex; height: 19.155px; width: 19.155px');
-        square.classList.add('square');
-        photoSketchPad.appendChild(square);
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
 
-        square.onmousemove = function(){
-            square.style.backgroundColor = 'transparent';
+// for (let i = 0; i < 120; i++) {
+//   const photoSketchPad = document.querySelector("#photoSketchPad");
+//   const square = document.createElement("div");
+//   square.setAttribute(
+//     "style",
+//     "display: flex; height: 19.155px; width: 19.155px"
+//   );
+//   square.classList.add("square");
+//   photoSketchPad.appendChild(square);
 
-
-        };
-
-};
-
-
+//   square.onmousemove = function () {
+//     square.style.backgroundColor = "transparent";
+//   };
+// }
