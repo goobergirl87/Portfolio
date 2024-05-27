ea6acc6318679c93854d4fc41f7ed149e21a36e9
goobergirl87 <isabelheidekorn@gmail.com>
2024-05-27 15:01:36 +0200

    Delete old trials

--- debbojangles.js
+++ debbojangles.js
@@ -1,20 +1,4 @@
-// let start = document.querySelector("#start");
-// let pageBottom = document.querySelector("#page-bottom");
-
-// start.addEventListener("click", function () {
-//   pageBottom.scrollIntoView();
-// });
-
-// document.querySelectorAll(".nav__link").forEach(function (el) {
-//   el.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute("href");
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-//   });
-// });
-
-// Smooth scrolling
+"use strict";
 
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   e.preventDefault();
@@ -25,41 +9,4 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
-
-//Reveal sections - not active, cause it doesn't work in Safari
-
-// const allSections = document.querySelectorAll(".section");
-
-// const revealSection = function (entries, observer) {
-//   const [entry] = entries;
-//   console.log(entry);
-
-//   if (!entry.isIntersecting) return;
-//   entry.target.classList.remove("section-hidden");
-//   observer.unobserve(entry.target);
-// };
-
-// const sectionObserver = new IntersectionObserver(revealSection, {
-//   root: null,
-//   treshold: 0.1,
-// });
-
-// allSections.forEach(function (section) {
-//   sectionObserver.observe(section);
-//   section.classList.add("section-hidden");
-// });
-
-// for (let i = 0; i < 120; i++) {
-//   const photoSketchPad = document.querySelector("#photoSketchPad");
-//   const square = document.createElement("div");
-//   square.setAttribute(
-//     "style",
-//     "display: flex; height: 19.155px; width: 19.155px"
-//   );
-//   square.classList.add("square");
-//   photoSketchPad.appendChild(square);
-
-//   square.onmousemove = function () {
-//     square.style.backgroundColor = "transparent";
-//   };
-// }
+Z;
